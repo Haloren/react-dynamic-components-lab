94cba52e17a700b66d381086102fc944d60ac578
Haloren <mckennakyler@gmail.com>
2021-02-17 19:28:25 -0700

    Done.

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -9,12 +9,13 @@ export default class ColorBox extends Component {
   }
 
   render() {
+    console.log("Color Box", this.props.opacity)
+    
     return (
-      <div className="color-box" style={{opacity: null /*replace null with the value*/}}>
+      <div className="color-box" style={{opacity: this.props.opacity}}>
         {/* your conditional code here! */}
+        { this.props.opacity > .2 ? <ColorBox opacity={this.props.opacity - 0.1 } /> : null }
       </div>
     )
   }
-
 }
-
--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,15 @@
 //your code here
+import React, { Component } from 'react';
+// import BlogPost from './BlogPost';
+
+export default class Comment extends Component {
+    
+    render() {
+        console.log("Commet", this.props)
+        return (
+            <div className="comment">
+                {this.props.commentText}
+            </div>
+        );
+    }
+}
